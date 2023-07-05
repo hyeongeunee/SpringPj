7db04a1677c8d27f8d0b6c4547246bfd0cff7f3b
hyeongeun <hhg7899@gmail.com>
2023-07-05 18:23:28 +0900

    개인 프로젝트 생성

---
+++ SpringPj/src/main/java/com/test/spring/HomeController.java
@@ -0,0 +1,22 @@
+package com.test.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    // 이 프로젝트의 최상위 경로 요청이 오면
+    @RequestMapping("/")
+    // 생성자에 임의로 객체를 선언할 수 있다.
+    public String home(HttpServletRequest request) {
+
+        return "home";
+    }
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
